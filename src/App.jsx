f787db80b5a26226d36b9d1fd465d849e4bc7333
834R807 <150474122+wtdunlap@users.noreply.github.com>
2024-07-21 23:34:23 -0400

    fixed deployment issue by using render instead of githubpages, removed dist folder

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
             <div className="flex flex-col p-4">
                 <Nav />
             </div>
-            <div className="flex justify-top content-center flex-col pt-4 sm:pr-4 sm:w-5/6 md:w-3/4 lg:w-2/3 xl:w-2/4 2xl:w-1/4">
+            <div className="flex justify-top content-center flex-col pt-4 sm:pr-4 sm:w-5/6 md:w-3/4 lg:w-2/3 xl:w-3/5">
                 <Outlet />
             </div>
         </div>
